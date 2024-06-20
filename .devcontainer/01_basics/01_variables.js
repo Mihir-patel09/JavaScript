b60039d057c256f5c673fe728801c171aae011f7
Devloper-mihir09 <159426603+Devloper-mihir09@users.noreply.github.com>
2024-06-20 17:30:49 +0000

    done Pt-1

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 173
+let accountEmail = "mihir@26321.com"
+var accountPassword = "12345"
+accountCity = "gujrat"
+let accountState;
+
+// accountId = 2 // not allowed
+accountEmail = "mihir@mp.com"
+accountPassword = "2485651"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
