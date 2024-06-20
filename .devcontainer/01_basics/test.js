8c1c82d5c54057c19ec947116a330443743df119
Devloper-mihir09 <159426603+Devloper-mihir09@users.noreply.github.com>
2024-06-20 17:12:09 +0000

    new work started

---
+++ .devcontainer/01_basics/test.js
@@ -0,0 +1,7 @@
+console.log("Mihir")
+const accountId = 173317
+let accountEmail = "mihir26321@gmail.com"
+var accountPassword = "12345"
+let accountstate;
+
+console.table([accountId,accountEmail,accountPassword,accountstate])
